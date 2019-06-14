dea28042c693dc62f9738a0d94dc11b949e20674
Teodor Nikolov <teodor.nikolov22@gmail.com>
2019-06-14 09:01:02 +0200

    Implement cosma dgemm on top of cblas

--- src/cosma/blas.h
+++ src/cosma/blas.h
@@ -2,6 +2,41 @@
 #define COSMA_BLAS_H
 #include <mpi.h>
 
+#ifdef COSMA_WITH_MKL
+#include <mkl_cblas.h>
+#else
+#include <cblas.h>
+#endif
+
+namespace cosma {
+void dgemm(const int M,
+           const int N,
+           const int K,
+           const double alpha,
+           const double *A,
+           const int lda,
+           const double *B,
+           const int ldb,
+           const double beta,
+           double *C,
+           const int ldc) {
+    cblas_dgemm(CBLAS_LAYOUT::CblasColMajor,
+                CBLAS_TRANSPOSE::CblasNoTrans,
+                CBLAS_TRANSPOSE::CblasNoTrans,
+                M,
+                N,
+                K,
+                alpha,
+                A,
+                lda,
+                B,
+                ldb,
+                beta,
+                C,
+                ldc);
+}
+} // namespace cosma
+
 namespace blas {
 
 extern "C" {
