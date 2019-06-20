79e4e29d1e9dbd95a5ab755d9820927b6e64eb93
Teodor Nikolov <teodor.nikolov22@gmail.com>
2019-06-20 10:48:45 +0200

    Tests and build scripts for Piz Daint

---
+++ scripts/schedule_tests_on_daint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -l
+#SBATCH --job-name=cosma_tests
+#SBATCH --constraint=mc
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=1
+#SBATCH --output=cosma_tests.out
+#SBATCH --error=cosma_tests.err
+
+module load daint-mc
+module load CMake
+module unload cray-libsci
+module load intel # defines $MKLROOT
+
+# Setup the compiler
+#
+export CC=`which cc`
+export CXX=`which CC`
+
+# Enable dynamic linking
+#
+export CRAYPE_LINK_TYPE=dynamic
+
+# Move to `build` directory if not there already
+#
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+COSMA_DIR=$( dirname $script_dir )
+mkdir -p ${COSMA_DIR}/build
+cd ${COSMA_DIR}/build
+
+# Build tests if not already built
+#
+make tests
+
+# Run tests
+#
+srun tests/test.multiply
+srun -n 1 tests/test.mapper
+srun -n 1 tests/test.strategy
