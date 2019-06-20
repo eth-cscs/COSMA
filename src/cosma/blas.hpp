272607052c859195ce44a331dbd5d1176194e420
Teodor Nikolov <teodor.nikolov22@gmail.com>
2019-06-20 13:19:20 +0200

    Add complex and single precision versions of DGEMM

--- src/cosma/blas.hpp
+++ src/cosma/blas.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <mpi.h>
+#include <complex>
 
 namespace cosma {
 
@@ -13,6 +13,42 @@ void dgemm(const int M,
            const int ldb,
            const double beta,
            double *C,
+           const int ldc);
+
+void dgemm(const int M,
+           const int N,
+           const int K,
+           const std::complex<double> alpha,
+           const std::complex<double> *A,
+           const int lda,
+           const std::complex<double> *B,
+           const int ldb,
+           const std::complex<double> beta,
+           std::complex<double> *C,
+           const int ldc);
+
+void dgemm(const int M,
+           const int N,
+           const int K,
+           const float alpha,
+           const float *A,
+           const int lda,
+           const float *B,
+           const int ldb,
+           const float beta,
+           float *C,
+           const int ldc);
+
+void dgemm(const int M,
+           const int N,
+           const int K,
+           const std::complex<float> alpha,
+           const std::complex<float> *A,
+           const int lda,
+           const std::complex<float> *B,
+           const int ldb,
+           const std::complex<float> beta,
+           std::complex<float> *C,
            const int ldc);
 
 } // namespace cosma
