36fd59536694cc11a121ab316d15ccd2bcfa8bb6
Marko Kabic <marko.kabic@cscs.ch>
2019-07-09 22:21:06 +0200

    Adding pgemm miniapp only if compiled with scalapack.

--- cmake/FindMKL.cmake
+++ cmake/FindMKL.cmake
@@ -20,9 +20,9 @@ include(FindPackageHandleStandardArgs)
 
 find_path(MKL_INCLUDE_DIR mkl.h
     HINTS
-        $ENV{MKLROOT}/include
-        ${MKL_ROOT}/include
-)
+    $ENV{MKLROOT}/include
+    ${MKL_ROOT}/include
+    )
 mark_as_advanced(MKL_INCLUDE_DIR)
 
 set(_mkl_libpath_suffix "lib/intel64")
@@ -42,70 +42,70 @@ function(__mkl_find_library _name)
     find_library(${_name}
         NAMES ${ARGN}
         HINTS ENV MKLROOT
-              ${MKL_ROOT}
+        ${MKL_ROOT}
         PATH_SUFFIXES ${_mkl_libpath_suffix}
-    )
+        )
     mark_as_advanced(${_name})
 endfunction()
 
 __mkl_find_library(MKL_CORE_LIB mkl_core)
 
 if(NOT MKL_THREADING)
-  __mkl_find_library(MKL_THREADING_LIB mkl_sequential)
+    __mkl_find_library(MKL_THREADING_LIB mkl_sequential)
 elseif(MKL_THREADING MATCHES "GOMP")
-  __mkl_find_library(MKL_THREADING_LIB mkl_gnu_thread)
+    __mkl_find_library(MKL_THREADING_LIB mkl_gnu_thread)
 elseif(MKL_THREADING MATCHES "IOMP")
-  __mkl_find_library(MKL_THREADING_LIB mkl_intel_thread)
+    __mkl_find_library(MKL_THREADING_LIB mkl_intel_thread)
 endif()
 
 if(NOT MKL_USE_64BIT_INTEGERS)
-  __mkl_find_library(MKL_INTERFACE_LIB mkl_intel_lp64)
+    __mkl_find_library(MKL_INTERFACE_LIB mkl_intel_lp64)
 else()
-  __mkl_find_library(MKL_INTERFACE_LIB mkl_intel_ilp64)
+    __mkl_find_library(MKL_INTERFACE_LIB mkl_intel_ilp64)
 endif()
 
 find_package_handle_standard_args(MKL 
-  DEFAULT_MSG  MKL_CORE_LIB
-               MKL_THREADING_LIB
-               MKL_INTERFACE_LIB
-               MKL_INCLUDE_DIR
-  )
+    DEFAULT_MSG  MKL_CORE_LIB
+    MKL_THREADING_LIB
+    MKL_INTERFACE_LIB
+    MKL_INCLUDE_DIR
+    )
 
 # ScaLAPACK
 # 
 if(MKL_MPI_TYPE)
-  __mkl_find_library(MKL_SCALAPACK_LIB mkl_scalapack_lp64)
-  if (MKL_MPI_TYPE MATCHES "MPICH")
-    __mkl_find_library(MKL_BLACS_LIB mkl_blacs_intelmpi_lp64)
-  elseif(MKL_MPI_TYPE MATCHES "OMPI")
-    __mkl_find_library(MKL_BLACS_LIB mkl_blacs_openmpi_lp64)
-  endif()
+    __mkl_find_library(MKL_SCALAPACK_LIB mkl_scalapack_lp64)
+    if (MKL_MPI_TYPE MATCHES "MPICH")
+        __mkl_find_library(MKL_BLACS_LIB mkl_blacs_intelmpi_lp64)
+    elseif(MKL_MPI_TYPE MATCHES "OMPI")
+        __mkl_find_library(MKL_BLACS_LIB mkl_blacs_openmpi_lp64)
+    endif()
 
-  find_package_handle_standard_args(MKL_SCALAPACK 
-    DEFAULT_MSG MKL_BLACS_LIB
-                MKL_SCALAPACK_LIB
-  )
+    find_package_handle_standard_args(MKL_SCALAPACK 
+        DEFAULT_MSG MKL_BLACS_LIB
+        MKL_SCALAPACK_LIB
+        )
 endif()
 
 if (MKL_FOUND AND NOT TARGET MKL::MKL)
     find_package(Threads REQUIRED)
     add_library(MKL::CORE UNKNOWN IMPORTED)
     set_target_properties(MKL::CORE PROPERTIES 
-      IMPORTED_LOCATION ${MKL_CORE_LIB}
-      INTERFACE_LINK_LIBRARIES Threads::Threads)
+        IMPORTED_LOCATION ${MKL_CORE_LIB}
+        INTERFACE_LINK_LIBRARIES Threads::Threads)
 
     if(MKL_THREADING)
-      find_package(OpenMP REQUIRED)
-      set_target_properties(MKL::CORE PROPERTIES 
-        INTERFACE_LINK_LIBRARIES OpenMP::OpenMP_CXX)
+        find_package(OpenMP REQUIRED)
+        set_target_properties(MKL::CORE PROPERTIES 
+            INTERFACE_LINK_LIBRARIES OpenMP::OpenMP_CXX)
     endif()
 
     if(MKL_MPI_TYPE)
-      add_library(MKL::BLACS UNKNOWN IMPORTED)
-      set_target_properties(MKL::BLACS PROPERTIES 
-        IMPORTED_LOCATION ${MKL_BLACS_LIB})
-      set_target_properties(MKL::CORE PROPERTIES 
-        INTERFACE_LINK_LIBRARIES MKL::BLACS)
+        add_library(MKL::BLACS UNKNOWN IMPORTED)
+        set_target_properties(MKL::BLACS PROPERTIES 
+            IMPORTED_LOCATION ${MKL_BLACS_LIB})
+        set_target_properties(MKL::CORE PROPERTIES 
+            INTERFACE_LINK_LIBRARIES MKL::BLACS)
     endif()
 
     add_library(MKL::THREADING UNKNOWN IMPORTED)
@@ -115,27 +115,27 @@ if (MKL_FOUND AND NOT TARGET MKL::MKL)
 
     add_library(MKL::BLAS_INTERFACE UNKNOWN IMPORTED)
     set_target_properties(MKL::BLAS_INTERFACE PROPERTIES 
-      IMPORTED_LOCATION ${MKL_INTERFACE_LIB}
-      INTERFACE_LINK_LIBRARIES MKL::THREADING)
+        IMPORTED_LOCATION ${MKL_INTERFACE_LIB}
+        INTERFACE_LINK_LIBRARIES MKL::THREADING)
 
     # The MKL::MKL target
     #
     add_library(MKL::MKL INTERFACE IMPORTED)
     set_target_properties(MKL::MKL PROPERTIES 
-      INTERFACE_INCLUDE_DIRECTORIES "${MKL_INCLUDE_DIR}"
-      INTERFACE_LINK_LIBRARIES MKL::BLAS_INTERFACE)
+        INTERFACE_INCLUDE_DIRECTORIES "${MKL_INCLUDE_DIR}"
+        INTERFACE_LINK_LIBRARIES MKL::BLAS_INTERFACE)
 
     if(MKL_MPI_TYPE)
-      add_library(MKL::SCALAPACK UNKNOWN IMPORTED)
-      set_target_properties(MKL::SCALAPACK PROPERTIES 
-        IMPORTED_LOCATION ${MKL_SCALAPACK_LIB}
-        INTERFACE_LINK_LIBRARIES MKL::BLAS_INTERFACE)
+        add_library(MKL::SCALAPACK UNKNOWN IMPORTED)
+        set_target_properties(MKL::SCALAPACK PROPERTIES 
+            IMPORTED_LOCATION ${MKL_SCALAPACK_LIB}
+            INTERFACE_LINK_LIBRARIES MKL::BLAS_INTERFACE)
 
-      set_target_properties(MKL::MKL PROPERTIES 
-        INTERFACE_LINK_LIBRARIES MKL::SCALAPACK)
+        set_target_properties(MKL::MKL PROPERTIES 
+            INTERFACE_LINK_LIBRARIES MKL::SCALAPACK)
     else()
-      set_target_properties(MKL::MKL PROPERTIES 
-        INTERFACE_LINK_LIBRARIES MKL::BLAS_INTERFACE)
+        set_target_properties(MKL::MKL PROPERTIES 
+            INTERFACE_LINK_LIBRARIES MKL::BLAS_INTERFACE)
     endif()
 endif()
 
