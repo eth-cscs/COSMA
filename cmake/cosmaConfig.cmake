ae8a42c850243deae291988010e6e6e34387b9d4
Teodor Nikolov <teodor.nikolov22@gmail.com>
2019-07-16 11:00:45 +0200

    Complete support for installation
    - Use GNUInstallDirs instead of custom dirs
    - Include the FindMKL module and other dependencies into the Config file
    - Note: private static dependencies still have to be propagated

--- cmake/cosmaConfig.cmake
+++
@@ -1,7 +0,0 @@
-if(NOT TARGET cosma::cosma)
-  include(CMakeFindDependencyMacro)
-  find_dependency(MPI)
-  find_dependency(MKL)
-
-  include("${CMAKE_CURRENT_LIST_DIR}/cosmaTargets.cmake")
-endif()
