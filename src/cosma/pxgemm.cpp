7573b367280beb94adceed86b4c3fa57103e2f81
Marko Kabic <marko.kabic@cscs.ch>
2019-12-09 14:43:22 +0100

    Improved pxgemm wrappers: now compatible with different versions of Fortran compilers.

--- src/cosma/pxgemm.cpp
+++ src/cosma/pxgemm.cpp
@@ -1,5 +1,7 @@
 #include <cosma/pgemm.hpp>
 #include <cosma/pxgemm.h>
+#include <mpi.h>
+#include <stdio.h>
 
 extern "C" {
 // Reimplement ScaLAPACK signatures functions
@@ -22,6 +24,14 @@ void pdgemm(const char* trans_a,
             const int* ic,
             const int* jc,
             const int* descc) {
+
+    // int rank;
+    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // if (rank == 0) {
+    //     printf("m = %d, n = %d, k = %d", *m, *n, *k);
+    //     fflush(stdout);
+    // }
+    // MPI_Barrier(MPI_COMM_WORLD);
 
     cosma::pgemm<double>(*trans_a,
                   *trans_b,
@@ -164,5 +174,146 @@ void pzgemm(const char* trans_a,
                      *ic,
                      *jc,
                      descc);
+}
+
+// *********************************************************************************
+// Same as previously, but with added underscore at the end.
+// This is used for fortran interfaces, in case fortran expects these symbols
+// *********************************************************************************
+
+void psgemm_(const char* trans_a, const char* transb, const int* m, const int* n, const int* k,
+        const float* alpha, const float* a, const int* ia, const int* ja, const int* desca,
+        const float* b, const int* ib, const int* jb, const int* descb, const float* beta,
+        float* c, const int* ic, const int* jc, const int* descc) {
+    psgemm(trans_a, transb, m, n, k,
+           alpha, a, ia, ja, desca,
+           b, ib, jb, descb,
+           beta, c, ic, jc, descc);
+}
+
+void pdgemm_(const char* trans_a, const char* transb, const int* m, const int* n, const int* k,
+        const double* alpha, const double* a, const int* ia, const int* ja, const int* desca,
+        const double* b, const int* ib, const int* jb, const int* descb, const double* beta,
+        double* c, const int* ic, const int* jc, const int* descc) {
+    pdgemm(trans_a, transb, m, n, k,
+           alpha, a, ia, ja, desca,
+           b, ib, jb, descb,
+           beta, c, ic, jc, descc);
+}
+
+void pcgemm_(const char* trans_a, const char* transb, const int* m, const int* n, const int* k,
+        const float _Complex* alpha, const float _Complex* a, const int* ia,
+        const int* ja, const int* desca, const float _Complex* b, const int* ib,
+        const int* jb, const int* descb, const float _Complex* beta,
+        float _Complex* c, const int* ic, const int* jc, const int* descc) {
+    pcgemm(trans_a, transb, m, n, k,
+           alpha, a, ia, ja, desca,
+           b, ib, jb, descb,
+           beta, c, ic, jc, descc);
+}
+
+void pzgemm_(const char* trans_a, const char* transb, const int* m, const int* n, const int* k,
+        const double _Complex* alpha, const double _Complex* a, const int* ia,
+        const int* ja, const int* desca, const double _Complex* b, const int* ib,
+        const int* jb, const int* descb, const double _Complex* beta,
+        double _Complex* c, const int* ic, const int* jc, const int* descc) {
+    pzgemm(trans_a, transb, m, n, k,
+           alpha, a, ia, ja, desca,
+           b, ib, jb, descb,
+           beta, c, ic, jc, descc);
+}
+
+// *********************************************************************************
+// Same as previously, but with double underscore at the end.
+// This is used for fortran interfaces, in case fortran expects these symbols
+// *********************************************************************************
+
+void psgemm__(const char* trans_a, const char* transb, const int* m, const int* n, const int* k,
+        const float* alpha, const float* a, const int* ia, const int* ja, const int* desca,
+        const float* b, const int* ib, const int* jb, const int* descb, const float* beta,
+        float* c, const int* ic, const int* jc, const int* descc) {
+    psgemm(trans_a, transb, m, n, k,
+           alpha, a, ia, ja, desca,
+           b, ib, jb, descb,
+           beta, c, ic, jc, descc);
+}
+
+void pdgemm__(const char* trans_a, const char* transb, const int* m, const int* n, const int* k,
+        const double* alpha, const double* a, const int* ia, const int* ja, const int* desca,
+        const double* b, const int* ib, const int* jb, const int* descb, const double* beta,
+        double* c, const int* ic, const int* jc, const int* descc) {
+    pdgemm(trans_a, transb, m, n, k,
+           alpha, a, ia, ja, desca,
+           b, ib, jb, descb,
+           beta, c, ic, jc, descc);
+}
+
+void pcgemm__(const char* trans_a, const char* transb, const int* m, const int* n, const int* k,
+        const float _Complex* alpha, const float _Complex* a, const int* ia,
+        const int* ja, const int* desca, const float _Complex* b, const int* ib,
+        const int* jb, const int* descb, const float _Complex* beta,
+        float _Complex* c, const int* ic, const int* jc, const int* descc) {
+    pcgemm(trans_a, transb, m, n, k,
+           alpha, a, ia, ja, desca,
+           b, ib, jb, descb,
+           beta, c, ic, jc, descc);
+}
+
+void pzgemm__(const char* trans_a, const char* transb, const int* m, const int* n, const int* k,
+        const double _Complex* alpha, const double _Complex* a, const int* ia,
+        const int* ja, const int* desca, const double _Complex* b, const int* ib,
+        const int* jb, const int* descb, const double _Complex* beta,
+        double _Complex* c, const int* ic, const int* jc, const int* descc) {
+    pzgemm(trans_a, transb, m, n, k,
+           alpha, a, ia, ja, desca,
+           b, ib, jb, descb,
+           beta, c, ic, jc, descc);
+}
+
+// *********************************************************************************
+// Same as previously, but CAPITALIZED.
+// This is used for fortran interfaces, in case fortran expects these symbols
+// *********************************************************************************
+
+void PSGEMM(const char* trans_a, const char* transb, const int* m, const int* n, const int* k,
+        const float* alpha, const float* a, const int* ia, const int* ja, const int* desca,
+        const float* b, const int* ib, const int* jb, const int* descb, const float* beta,
+        float* c, const int* ic, const int* jc, const int* descc) {
+    psgemm(trans_a, transb, m, n, k,
+           alpha, a, ia, ja, desca,
+           b, ib, jb, descb,
+           beta, c, ic, jc, descc);
+}
+
+void PDGEMM(const char* trans_a, const char* transb, const int* m, const int* n, const int* k,
+        const double* alpha, const double* a, const int* ia, const int* ja, const int* desca,
+        const double* b, const int* ib, const int* jb, const int* descb, const double* beta,
+        double* c, const int* ic, const int* jc, const int* descc) {
+    pdgemm(trans_a, transb, m, n, k,
+           alpha, a, ia, ja, desca,
+           b, ib, jb, descb,
+           beta, c, ic, jc, descc);
+}
+
+void PCGEMM(const char* trans_a, const char* transb, const int* m, const int* n, const int* k,
+        const float _Complex* alpha, const float _Complex* a, const int* ia,
+        const int* ja, const int* desca, const float _Complex* b, const int* ib,
+        const int* jb, const int* descb, const float _Complex* beta,
+        float _Complex* c, const int* ic, const int* jc, const int* descc) {
+    pcgemm(trans_a, transb, m, n, k,
+           alpha, a, ia, ja, desca,
+           b, ib, jb, descb,
+           beta, c, ic, jc, descc);
+}
+
+void PZGEMM(const char* trans_a, const char* transb, const int* m, const int* n, const int* k,
+        const double _Complex* alpha, const double _Complex* a, const int* ia,
+        const int* ja, const int* desca, const double _Complex* b, const int* ib,
+        const int* jb, const int* descb, const double _Complex* beta,
+        double _Complex* c, const int* ic, const int* jc, const int* descc) {
+    pzgemm(trans_a, transb, m, n, k,
+           alpha, a, ia, ja, desca,
+           b, ib, jb, descb,
+           beta, c, ic, jc, descc);
 }
 }
