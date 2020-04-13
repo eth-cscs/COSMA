613087f2517b632196817cb102a3bd2007143288
Marko Kabic <mkmarkokabic@gmail.com>
2020-04-14 01:37:43 +0200

    Reduced memory usage.

--- src/cosma/memory_pool.cpp
+++ src/cosma/memory_pool.cpp
@@ -91,7 +91,7 @@ void cosma::memory_pool<T>::reserve(size_t size) {
         // function from this allocator might actually allocate
         // a bit more (due to the alignment) bytes than instructed to, 
         // so we have to take that into account.
-        std::size_t size_with_alignment = (pool_capacity_ + size) + main_allocator<T>::alignment;
+        std::size_t size_with_alignment = (pool_size_ + size) + main_allocator<T>::alignment;
         pool_.reserve(size_with_alignment);
     }
 }
