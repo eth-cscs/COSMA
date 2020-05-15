4e41bf3ef6cdc0a19bdf3ba1b584ba733ccedfad
Harmen Stoppels <harmenstoppels@gmail.com>
2020-05-15 22:40:41 +0200

    Rename functions and change struct of arrays to array to structs

--- src/cosma/cinterface.cpp
+++ src/cosma/cinterface.cpp
@@ -1,39 +1,29 @@
 #include <grid2grid/grid2D.hpp>
-#include <grid2grid/ranks_reordering.hpp>
-#include <grid2grid/transformer.hpp>
 
 #include <cosma/cinterface.hpp>
-#include <cosma/context.hpp>
-#include <cosma/mapper.hpp>
-#include <cosma/matrix.hpp>
 #include <cosma/multiply.hpp>
 
 #include <mpi.h>
 
-using namespace cosma;
-
 namespace cosma {
 
 template <class T>
-grid2grid::grid_layout<T> grid_from_clayout(int n_ranks,
-                                            const ::layout *layout) {
-    T **localblock_data = reinterpret_cast<T **>(layout->localblock_data);
+grid2grid::grid_layout<T> grid_from_clayout(int n_ranks, const ::layout *layout) {
 
     // Create the local blocks
     std::vector<grid2grid::block<T>> loc_blks;
 
-    // Struct of vectors -> vector of structs basically
-    for (int i = 0; i < layout->nlocalblock; ++i) {
-        auto row_block_idx = layout->localblock_row[i];
-        auto col_block_idx = layout->localblock_col[i];
-        T *ptr = localblock_data[i];
-        auto stride = layout->localblock_ld[i];
+    // Create blocks
+    for (int i = 0; i < layout->nlocalblocks; ++i) {
+        auto &block = layout->localblocks[i];
+        auto row = block.row;
+        auto col = block.col;
+        auto ptr = reinterpret_cast<T *>(block.data);
+        auto stride = block.ld;
 
-        grid2grid::block_coordinates coord{row_block_idx, col_block_idx};
-        grid2grid::interval rows{layout->rowsplit[row_block_idx],
-                                 layout->rowsplit[row_block_idx + 1]};
-        grid2grid::interval cols{layout->colsplit[col_block_idx],
-                                 layout->colsplit[col_block_idx + 1]};
+        grid2grid::block_coordinates coord{row, col};
+        grid2grid::interval rows{layout->rowsplit[row], layout->rowsplit[row + 1]};
+        grid2grid::interval cols{layout->colsplit[col], layout->colsplit[col + 1]};
         loc_blks.emplace_back(rows, cols, coord, ptr, stride);
     }
 
@@ -58,7 +48,7 @@ grid2grid::grid_layout<T> grid_from_clayout(int n_ranks,
 }
 
 template <class T>
-void pxmultiply(MPI_Comm comm,
+void xmultiply_using_layout(MPI_Comm comm,
                 const char *transa,
                 const char *transb,
                 const T *alpha,
@@ -80,7 +70,7 @@ void pxmultiply(MPI_Comm comm,
     cosma_layout_b.transpose_or_conjugate(std::toupper(*transb));
 
     // perform cosma multiplication
-    multiply_using_layout<T>(
+    cosma::multiply_using_layout<T>(
         cosma_layout_a, cosma_layout_b, cosma_layout_c, *alpha, *beta, comm);
 }
 } // namespace cosma
@@ -89,7 +79,7 @@ void pxmultiply(MPI_Comm comm,
 extern "C" {
 #endif
 
-void psmultiply(MPI_Comm comm,
+void smultiply_using_layout(MPI_Comm comm,
                 const char *transa,
                 const char *transb,
                 const float *alpha,
@@ -98,11 +88,11 @@ void psmultiply(MPI_Comm comm,
                 const float *beta,
                 const layout *layout_c) {
 
-    pxmultiply<float>(
+    cosma::xmultiply_using_layout<float>(
         comm, transa, transb, alpha, layout_a, layout_b, beta, layout_c);
 }
 
-void pdmultiply(MPI_Comm comm,
+void dmultiply_using_layout(MPI_Comm comm,
                 const char *transa,
                 const char *transb,
                 const double *alpha,
@@ -111,11 +101,11 @@ void pdmultiply(MPI_Comm comm,
                 const double *beta,
                 const layout *layout_c) {
 
-    pxmultiply<double>(
+    cosma::xmultiply_using_layout<double>(
         comm, transa, transb, alpha, layout_a, layout_b, beta, layout_c);
 }
 
-void pcmultiply(MPI_Comm comm,
+void cmultiply_using_layout(MPI_Comm comm,
                 const char *transa,
                 const char *transb,
                 const float *alpha,
@@ -124,7 +114,7 @@ void pcmultiply(MPI_Comm comm,
                 const float *beta,
                 const layout *layout_c) {
 
-    pxmultiply<std::complex<float>>(
+    cosma::xmultiply_using_layout<std::complex<float>>(
         comm,
         transa,
         transb,
@@ -135,7 +125,7 @@ void pcmultiply(MPI_Comm comm,
         layout_c);
 }
 
-void pzmultiply(MPI_Comm comm,
+void zmultiply_using_layout(MPI_Comm comm,
                 const char *transa,
                 const char *transb,
                 const double *alpha,
@@ -144,7 +134,7 @@ void pzmultiply(MPI_Comm comm,
                 const double *beta,
                 const layout *layout_c) {
 
-    pxmultiply<std::complex<double>>(
+    cosma::xmultiply_using_layout<std::complex<double>>(
         comm,
         transa,
         transb,
--- src/cosma/cinterface.hpp
+++ src/cosma/cinterface.hpp
@@ -6,20 +6,40 @@
 extern "C" {
 #endif
 
+/**
+ * A local block of the matrix.
+ * data: a pointer to the start of the local matrix A_loc
+ * ld: leading dimension or distance between two columns of A_loc
+ * row: the global block row index
+ * col: the global block colum index
+ */
+struct block {
+    void *data;
+    const int ld;
+    const int row;
+    const int col;
+};
+
+/**
+ * Description of a distributed layout of a matrix
+ * rowblocks: number of gobal blocks
+ * colblocks: number of gobal blocks
+ * rowsplit: [rowsplit[i], rowsplit[i+1]) is range of rows of block i
+ * colsplit: [colsplit[i], colsplit[i+1]) is range of columns of block i
+ * nlocalblock: number of blocks owned by the current rank
+ * localblcoks: an array of block descriptions of the current rank
+ */
 struct layout {
     int rowblocks;
     int colblocks;
     const int *rowsplit;
     const int *colsplit;
     const int *owners;
-    int nlocalblock;
-    void **localblock_data;
-    const int *localblock_ld;
-    const int *localblock_row;
-    const int *localblock_col;
+    int nlocalblocks;
+    block *localblocks;
 };
 
-void psmultiply(MPI_Comm comm,
+void smultiply_using_layout(MPI_Comm comm,
                 const char *transa,
                 const char *transb,
                 const float *alpha,
@@ -28,7 +48,7 @@ void psmultiply(MPI_Comm comm,
                 const float *beta,
                 const layout *layout_c);
 
-void pdmultiply(MPI_Comm comm,
+void dmultiply_using_layout(MPI_Comm comm,
                 const char *transa,
                 const char *transb,
                 const double *alpha,
@@ -37,7 +57,7 @@ void pdmultiply(MPI_Comm comm,
                 const double *beta,
                 const layout *layout_c);
 
-void pcmultiply(MPI_Comm comm,
+void cmultiply_using_layout(MPI_Comm comm,
                 const char *transa,
                 const char *transb,
                 const float *alpha,
@@ -46,7 +66,7 @@ void pcmultiply(MPI_Comm comm,
                 const float *beta,
                 const layout *layout_c);
 
-void pzmultiply(MPI_Comm comm,
+void zmultiply_using_layout(MPI_Comm comm,
                 const char *transa,
                 const char *transb,
                 const double *alpha,
