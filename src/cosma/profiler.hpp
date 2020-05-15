f6258b9077f7a9ba00bb664d8460905b634d0dd2
Marko Kabic <mkmarkokabic@gmail.com>
2020-05-15 20:56:25 +0200

    Updated submodules.

--- src/cosma/profiler.hpp
+++ src/cosma/profiler.hpp
@@ -3,7 +3,14 @@
 // The header makes semiprof an optional dependency that needs not be shipped when COSMA is installed.
 //
 #ifdef COSMA_WITH_PROFILING
+
 #include <semiprof/semiprof.hpp>
+
+// prints the profiler summary
+#define PP() std::cout << semiprof::profiler_summary() << "\n"
+// clears the profiler (counts and timings)
+#define PC() semiprof::profiler_clear()
+
 #else
 #define PE(name)
 #define PL()
