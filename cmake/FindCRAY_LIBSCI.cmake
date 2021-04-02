dfd052e355e7b5bca5bbc3d0344d0524dc324804
Marko Kabic <marko.kabic@cscs.ch>
2021-04-02 14:06:23 +0200

    Added the Clang compiler support for the Cray-libsci backend.

--- cmake/FindCRAY_LIBSCI.cmake
+++ cmake/FindCRAY_LIBSCI.cmake
@@ -1,18 +1,24 @@
 include(FindPackageHandleStandardArgs)
-
-# we are using the GNU compiler
-set(_sciname "sci_gnu_mpi_mp")
-set(_sciname_acc "sci_acc_gnu_nv60")
-
+if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+    set(_sciname "sci_gnu_mpi_mp")
+elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "Intel")
+    set(_sciname "sci_intel_mpi_mp")
+elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+    set(_sciname "sci_cray_mpi_mp")
+else()
+    message(${CMAKE_CXX_COMPILER_ID})
+    message(FATAL_ERROR "Unknown compiler. When using cray-libsci use either GNU, INTEL or Clang compiler")
+endif()
 find_library(CRAY_LIBSCI_LIBRARIES
     NAMES ${_sciname_acc} ${_sciname}
     HINTS
     ${_SCALAPACK_LIBRARY_DIRS}
+    ENV SCALAPACKROOT
+    ENV CRAY_LIBSCI_PREFIX_DIR
     ENV CRAY_LIBSCI_PREFIX_DIR
     ENV CRAY_LIBSCI_ACC_PREFIX_DIR
     PATH_SUFFIXES lib
-    DOC "Path to the Cray-libsci library.")
+    DOC "Path to the Cray-libsci library")
 
 message("CRAY_LIBSCI: ${CRAY_LIBSCI_LIBRARIES}")
-
 find_package_handle_standard_args(CRAY_LIBSCI DEFAULT_MSG CRAY_LIBSCI_LIBRARIES)
