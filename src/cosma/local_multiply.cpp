fdcf2f1d94a0f8660df448562a2f00f39c76b5f0
Dr. Mathieu Taillefumier <mathieu.taillefumier@free.fr>
2023-08-18 10:47:10 +0200

    Enable unified memory

--- src/cosma/local_multiply.cpp
+++ src/cosma/local_multiply.cpp
@@ -6,6 +6,10 @@
 #ifdef COSMA_HAVE_GPU
 #include <Tiled-MM/tiled_mm.hpp>
 #include <Tiled-MM/util.hpp>
+
+#ifdef COSMA_USE_UNIFIED_MEMORY
+#include <Tiled-MM/gpu_blas_api.hpp>
+#endif
 #endif
 
 #if defined(COSMA_WITH_BLAS) || defined(COSMA_WITH_MKL_BLAS)
@@ -20,71 +24,187 @@
 
 namespace cosma {
 
+
 using clock_t = std::chrono::high_resolution_clock;
 using ms_t = std::chrono::milliseconds;
 
-template <typename Scalar>
-void print_matrix(int m, int n, Scalar *A, char label) {
-    std::cout << "Matrix " << label << std::endl;
-    for (int i = 0; i < m; ++i) {
+#ifdef COSMA_USE_UNIFIED_MEMORY
+  using zfloat = std::complex<float>;
+  using zdouble = std::complex<double>;
+  
+  int get_first(char trans, int m, int n) {
+    return trans == 'N' ? m : n;
+  }
+  
+  int get_second(char trans, int m, int n) {
+    return trans == 'N' ? n : m;
+  }
+  
+  gpu::blas_api::OperationType get_blas_operation(char trans) {
+    gpu::blas_api::OperationType op = trans == 'T'
+      ?
+      gpu::blas_api::operation::Transpose
+      :
+      (trans == 'C'
+       ?
+       gpu::blas_api::operation::ConjugateTranspose
+       :
+       gpu::blas_api::operation::None);
+    return op;
+  }
+  
+  gpu::blas_api::StatusType cublas_gemm_wrapper(gpu::blas_api::HandleType handle,
+                                                char trans_a, char trans_b,
+                                                int m, int n, int k,
+                                                const float* alpha,
+                                                const float* a,
+                                                const float* b,
+                                                const float* beta,
+                                                float* c,
+                                                int lld_c) {
+    gpu::blas_api::OperationType op_a = get_blas_operation(trans_a);
+    gpu::blas_api::OperationType op_b = get_blas_operation(trans_b);
+    
+    int ld_a = get_first(trans_a, m , k);
+    int ld_b = get_first(trans_b, k , n);
+    
+    return gpu::blas_api::sgemm(handle, op_a, op_b, m, n, k,
+                                alpha, a, ld_a, b, ld_b, beta, c, lld_c);
+  }
+  
+  gpu::blas_api::StatusType cublas_gemm_wrapper(gpu::blas_api::HandleType handle,
+                                                char trans_a, char trans_b,
+                                                int m, int n, int k,
+                                                const double* alpha,
+                                                const double* a,
+                                                const double* b,
+                                                const double* beta,
+                                                double* c,
+                                                int lld_c) {
+    gpu::blas_api::OperationType op_a = get_blas_operation(trans_a);
+    gpu::blas_api::OperationType op_b = get_blas_operation(trans_b);
+    
+    int ld_a = get_first(trans_a, m , k);
+    int ld_b = get_first(trans_b, k , n);
+    
+    return gpu::blas_api::dgemm(handle, op_a, op_b, m, n, k,
+                                alpha, a, ld_a, b, ld_b, beta, c, lld_c);
+  }
+  
+  // Note: Converting from std::complex to cuComplex and cuDoubleComple
+  //       works because they are binary compatible.
+  //
+  //       http://icl.cs.utk.edu/magma/forum/viewtopic.php?f=2&t=902
+  //
+  gpu::blas_api::StatusType cublas_gemm_wrapper(gpu::blas_api::HandleType handle,
+                                                char trans_a, char trans_b,
+                                                int m, int n, int k,
+                                                const zfloat* alpha,
+                                                const zfloat* a,
+                                                const zfloat* b,
+                                                const zfloat* beta,
+                                                zfloat* c,
+                                                int lld_c) {
+    gpu::blas_api::OperationType op_a = get_blas_operation(trans_a);
+    gpu::blas_api::OperationType op_b = get_blas_operation(trans_b);
+    
+    int ld_a = get_first(trans_a, m , k);
+    int ld_b = get_first(trans_b, k , n);
+    
+    return gpu::blas_api::cgemm(handle, op_a, op_b, m, n, k,
+                                reinterpret_cast<const gpu::blas_api::ComplexFloatType*>(alpha),
+                                reinterpret_cast<const gpu::blas_api::ComplexFloatType*>(a), ld_a,
+                                reinterpret_cast<const gpu::blas_api::ComplexFloatType*>(b), ld_b,
+                                reinterpret_cast<const gpu::blas_api::ComplexFloatType*>(beta),
+                                reinterpret_cast<gpu::blas_api::ComplexFloatType*>(c), lld_c);
+  }
+
+  gpu::blas_api::StatusType cublas_gemm_wrapper(gpu::blas_api::HandleType handle,
+                                                char trans_a, char trans_b,
+                                                int m, int n, int k,
+                                                const zdouble* alpha,
+                                                const zdouble* a,
+                                                const zdouble* b,
+                                                const zdouble* beta,
+                                                zdouble* c,
+                                                int lld_c) {
+    gpu::blas_api::OperationType op_a = get_blas_operation(trans_a);
+    gpu::blas_api::OperationType op_b = get_blas_operation(trans_b);
+
+    int ld_a = get_first(trans_a, m , k);
+    int ld_b = get_first(trans_b, k , n);
+
+    return gpu::blas_api::zgemm(handle, op_a, op_b, m, n, k,
+                                reinterpret_cast<const gpu::blas_api::ComplexDoubleType*>(alpha),
+                                reinterpret_cast<const gpu::blas_api::ComplexDoubleType*>(a), ld_a,
+                                reinterpret_cast<const gpu::blas_api::ComplexDoubleType*>(b), ld_b,
+                                reinterpret_cast<const gpu::blas_api::ComplexDoubleType*>(beta),
+                                reinterpret_cast<gpu::blas_api::ComplexDoubleType*>(c), lld_c);
+  }
+#endif
+
+    template <typename Scalar>
+      void print_matrix(int m, int n, Scalar *A, char label) {
+      std::cout << "Matrix " << label << std::endl;
+      for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
-            std::cout << A[j * m + i] << " ";
+          std::cout << A[j * m + i] << " ";
         }
         std::cout << std::endl;
+      }
+      std::cout << std::endl;
     }
-    std::cout << std::endl;
-}
 
-template <typename Scalar>
-clock_t::time_point debug_gemm_start(Scalar *matrixA,
-                                     Scalar *matrixB,
-                                     Scalar *matrixC,
-                                     int m,
-                                     int n,
-                                     int k,
-                                     Scalar alpha,
-                                     Scalar beta) {
-    auto start = clock_t::now();
-    if (std::abs(beta) > 0) {
+    template <typename Scalar>
+      clock_t::time_point debug_gemm_start(Scalar *matrixA,
+                                           Scalar *matrixB,
+                                           Scalar *matrixC,
+                                           int m,
+                                           int n,
+                                           int k,
+                                           Scalar alpha,
+                                           Scalar beta) {
+      auto start = clock_t::now();
+      if (std::abs(beta) > 0) {
         std::cout << "C (before) = " << std::endl;
         print_matrix(m, n, matrixC, 'C');
         auto C_partial = std::unique_ptr<Scalar[]>(new Scalar[m * n]);
         gemm(m, n, k, alpha, matrixA, m, matrixB, k, 0.0, C_partial.get(), m);
         std::cout << "C (partial) = " << std::endl;
         print_matrix(m, n, C_partial.get(), 'C');
+      }
+      return start;
     }
-    return start;
-}
 
-template <typename Scalar>
-clock_t::time_point debug_gemm_end(Scalar *matrixA,
-                                   Scalar *matrixB,
-                                   Scalar *matrixC,
-                                   int m,
-                                   int n,
-                                   int k,
-                                   Scalar alpha,
-                                   Scalar beta) {
-    std::cout << "After multiplication: " << std::endl;
-    std::cout << "beta = " << beta << std::endl;
-    print_matrix(m, k, matrixA, 'A');
-    print_matrix(k, n, matrixB, 'B');
-    print_matrix(m, n, matrixC, 'C');
+    template <typename Scalar>
+      clock_t::time_point debug_gemm_end(Scalar *matrixA,
+                                         Scalar *matrixB,
+                                         Scalar *matrixC,
+                                         int m,
+                                         int n,
+                                         int k,
+                                         Scalar alpha,
+                                         Scalar beta) {
+      std::cout << "After multiplication: " << std::endl;
+      std::cout << "beta = " << beta << std::endl;
+      print_matrix(m, k, matrixA, 'A');
+      print_matrix(k, n, matrixB, 'B');
+      print_matrix(m, n, matrixC, 'C');
 
-    return std::chrono::high_resolution_clock::now();
-}
+      return std::chrono::high_resolution_clock::now();
+    }
 
 #ifdef COSMA_HAVE_GPU
-template <typename Scalar>
-void local_multiply(gpu::mm_handle<Scalar>* gpu_ctx,
-                    Scalar *matrixA,
-                    Scalar *matrixB,
-                    Scalar *matrixC,
-                    int m,
-                    int n,
-                    int k,
-                    Scalar alpha,
-                    Scalar beta,
+    template <typename Scalar>
+      void local_multiply(gpu::mm_handle<Scalar>* gpu_ctx,
+                          Scalar *matrixA,
+                          Scalar *matrixB,
+                          Scalar *matrixC,
+                          int m,
+                          int n,
+                          int k,
+                          Scalar alpha,
+                          Scalar beta,
                     bool pin_host_buffers,
                     bool copy_c_back) {
     /*
@@ -100,7 +220,21 @@ void local_multiply(gpu::mm_handle<Scalar>* gpu_ctx,
     int ld_b = k;
     int ld_c = m;
 
+#ifndef COSMA_USE_UNIFIED_MEMORY
     gpu::gemm(*gpu_ctx, 'N', 'N', m, n, k, alpha, matrixA, ld_a, matrixB, ld_b, beta, matrixC, ld_c, pin_host_buffers, copy_c_back);
+#else
+
+    auto status = cublas_gemm_wrapper((*gpu_ctx).get_gpu_context().get_blas_handle(0),
+                                      'N', 'N',
+                                      m, n, k,
+                                      &alpha,
+                                      matrixA,
+                                      matrixB,
+                                      &beta,
+                                      matrixC,
+                                      m);
+    //gpu::gpu::blas_api::check_blas_status(status);
+#endif
     /*
     if (rank == 0) {
         gpu::copy_to_host(gpu_ctx->get_full_device_buffer_c().data(), matrixC, m * n);
