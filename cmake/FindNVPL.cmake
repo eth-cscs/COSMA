1aa332f4536247ec035333d9f613fbb47e93673e
Rocco Meli <rocco.meli@cscs.ch>
2025-03-05 14:26:55 +0100

    add missing include

--- cmake/FindNVPL.cmake
+++ cmake/FindNVPL.cmake
@@ -13,8 +13,16 @@ else()
   set(_nvpl_thread "_seq")
 endif()
 
-if(NOT TARGET "cosma::BLAS::NVPL::scalapack_link")
-  add_library("cosma::BLAS::NVPL::scalapack_link" INTERFACE IMPORTED)
-  target_link_libraries("cosma::BLAS::NVPL::scalapack_link" INTERFACE
+if(NOT TARGET "cosma::BLAS::NVPL::nvpl")
+  add_library("cosma::BLAS::NVPL::nvpl" INTERFACE IMPORTED)
+  target_link_libraries("cosma::BLAS::NVPL::nvpl" INTERFACE
     "nvpl::blas${_nvpl_int}${_nvpl_thread}" "nvpl::lapack${_nvpl_int}${_nvpl_thread}")
+  get_target_property(COSMA_NVPL_BLAS_LIBRARIES "nvpl::blas${_nvpl_int}${_nvpl_thread}" nvpl_blas_LIBRARY_DIR)
+  get_target_property(COSMA_NVPL_LAPACK_LIBRARIES "nvpl::lapack${_nvpl_int}${_nvpl_thread}" nvpl_lapack_LIBRARY_DIR)
+
+  add_library(cosma::BLAS::NVPL::blas ALIAS cosma::BLAS::NVPL::nvpl)
+  add_library(cosma::BLAS::NVPL::scalapack_link INTERFACE IMPORTED)
+  set_target_properties(
+    cosma::BLAS::NVPL::scalapack_link 
+    PROPERTIES INTERFACE_LINK_LIBRARIES "${COSMA_NVPL_BLAS_LIBRARIES};${COSMA_NVPL_LAPACK_LIBRARIES}")
 endif()
